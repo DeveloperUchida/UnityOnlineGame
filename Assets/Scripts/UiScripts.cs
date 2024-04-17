71b689939421083d1b53641aa9f81dacf8a24114
DeveloperUchida <kouei28poiou@icloud.com>
2024-04-17 13:24:51 +0900

    DataUpdate 2024/04/17

--- Assets/Scripts/UiScripts.cs
+++ Assets/Scripts/UiScripts.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIScripts : MonoBehaviour
 {
+    private Text ScoreText;
+    public int Score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScoreText = GetComponentInChildren<Text>();
+        ScoreText.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScoreText.text = Score.ToString();
     }
 }
